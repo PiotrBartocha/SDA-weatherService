76a9ac94a19c00b48d6f8bdc9c7af5e717c6fd80
Piotr <pbartocha@gmail.com>
2021-01-23 17:16:15 +0100

    UI template completed

--- src/main/java/WeatherReportApp/WeatherReport.java
+++ src/main/java/WeatherReportApp/WeatherReport.java
@@ -1,17 +1,61 @@
 package WeatherReportApp;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class WeatherReport {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Welcome to WeatherReport!");
-        System.out.println("Please select action to perform by putting the corresponding number");
+        System.out.println();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("You can choose one of the following actions");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println();
         System.out.println("1. Add a new city to the report");
         System.out.println("2. Display all cities from the database");
-        System.out.println("3. Get datas from weather services");
+        System.out.println("3. Get data from weather services");
         System.out.println("4. Close the app");
+        System.out.println();
 
+        String action = getActionFromUser();
+
+        switch (action) {
+            case "1": {
+                //todo
+            }
+            case "2": {
+                //todo
+            }
+
+            case "3": {
+                //todo
+            }
+            case "4": {
+                // todo
+
+            }
+
+        }
 
 
     }
+
+
+    private static String getActionFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        String actionID;
+
+        do {
+            System.out.println("Please choose the action number");
+            actionID = scanner.nextLine();
+        } while (!actionID.equals("1") && !actionID.equals("2") && !actionID.equals("3") && !actionID.equals("4"));
+
+
+        return actionID;
+    }
+
+
 }
+
+
