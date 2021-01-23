1dc8a5b64abc2919737985e550eee13ce3d67f02
Piotr <pbartocha@gmail.com>
2021-01-23 14:11:18 +0100

    main added

---
+++ src/main/java/WeatherReportApp/WeatherReport.java
@@ -0,0 +1,17 @@
+package WeatherReportApp;
+
+import java.util.Scanner;
+
+public class WeatherReport {
+    public static void main(String[] args) {
+        System.out.println("Welcome to WeatherReport!");
+        System.out.println("Please select action to perform by putting the corresponding number");
+        System.out.println("1. Add a new city to the report");
+        System.out.println("2. Display all cities from the database");
+        System.out.println("3. Get datas from weather services");
+        System.out.println("4. Close the app");
+
+
+
+    }
+}
