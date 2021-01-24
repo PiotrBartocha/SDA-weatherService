533bf6615e42de93a10c9aac12738134942246b0
Piotr <pbartocha@gmail.com>
2021-01-24 09:42:30 +0100

    UI template completed

--- src/main/java/WeatherReportApp/WeatherReport.java
+++ src/main/java/WeatherReportApp/WeatherReport.java
@@ -6,33 +6,39 @@ import java.util.concurrent.TimeUnit;
 
 public class WeatherReport {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Welcome to WeatherReport!");
-        System.out.println();
-        TimeUnit.SECONDS.sleep(1);
-        System.out.println("You can choose one of the following actions");
-        TimeUnit.SECONDS.sleep(1);
-        System.out.println();
-        System.out.println("1. Add a new city to the report");
-        System.out.println("2. Display all cities from the database");
-        System.out.println("3. Get data from weather services");
-        System.out.println("4. Close the app");
-        System.out.println();
+        while (true) {
+            System.out.println("Welcome to WeatherReport!");
+            System.out.println();
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println("You can choose one of the following actions");
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println();
+            System.out.println("1. Add a new city to the report");
+            System.out.println("2. Display all cities from the database");
+            System.out.println("3. Get data from weather services");
+            System.out.println("4. Close the app");
+            System.out.println();
 
-        String action = getActionFromUser();
+            String action = getActionFromUser();
 
-        switch (action) {
-            case "1": {
-                //todo add break;
-            }
-            case "2": {
-                //todo add break;
-            }
+            switch (action) {
+                case "1":
+                    addNewLocation();
+                    break;//todo
 
-            case "3": {
-                //todo add break;
-            }
-            case "4": {
-                System.out.println("Allright! Bye!");
+                case "2":
+                    displayAllLocations();
+                    break;//todo
+
+
+                case "3":
+                    getDataFromWeatherServices();
+                    break;//todo
+
+                case "4":
+                    System.out.println("Allright! Bye!");
+                    return;
+
             }
         }
     }
@@ -48,6 +54,15 @@ public class WeatherReport {
         } while (!actionID.equals("1") && !actionID.equals("2") && !actionID.equals("3") && !actionID.equals("4")); // todo List.of(1,2,3,4).contains(...)
 
         return actionID;
+    }
+
+    private static void addNewLocation() {
+    }
+
+    private static void displayAllLocations() {
+    }
+
+    private static void getDataFromWeatherServices() {
     }
 }
 
