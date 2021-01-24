b17ea8c3bce506da857b11b4e5128ec614ac02d2
Piotr <pbartocha@gmail.com>
2021-01-24 14:41:37 +0100

    updated "new location" feature still needs input data validators

--- src/main/java/backend/LocationService.java
+++ src/main/java/backend/LocationService.java
@@ -10,15 +10,39 @@ public class LocationService {
     public Location createNewLocation(String name, String latitude, String longitude, String country, String region) {
         // todo do a validation
         float latitudeFloat;
-
+        float longitudeFloat;
         try {
             latitudeFloat = Float.parseFloat(latitude);
         } catch (NumberFormatException e) {
-            throw new RuntimeException("Zły format wartości latitude: " + latitude);
+            throw new RuntimeException("Wrong latitude value format: " + latitude);
+        }
+        if (latitudeFloat > 90) {
+            throw new RuntimeException("Latitude cannot be higher than 90!");
         }
 
-        if (latitudeFloat > 90) {
-            throw new RuntimeException("Wartość latitude nie powinna być większa niż 90");
+        try {
+            longitudeFloat = Float.parseFloat(longitude);
+
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Wrong longitude value format: " + longitude);
+        }
+        if (longitudeFloat > 180) {
+            throw new RuntimeException("Longitude cannot be higher than 180!");
+        }
+        if (name.equals(null) || name.isBlank()) {
+            throw new RuntimeException("Name cannot be empty!");
+        }
+        if (country.equals(null) || country.isBlank()) {
+            throw new RuntimeException("Country name cannot be empty");
+        }
+        if (name.length() > 30) {
+            throw new RuntimeException("City name cannot be longer than 20 characters!");
+        }
+        if (country.length() > 30) {
+            throw new RuntimeException("Country name cannot be longer than 20 characters!");
+        }
+        if (!name.matches("[a-zA-Z\\s\\-]")) {
+            throw new RuntimeException("Name contains illegal characters");
         }
 
         Location location = new Location(name, latitude, longitude, country, region);
