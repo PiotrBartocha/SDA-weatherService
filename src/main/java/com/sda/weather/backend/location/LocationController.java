375b2235a5b11f42ab6d8a634b6737c078a5d803
Piotr <pbartocha@gmail.com>
2021-01-30 11:33:13 +0100

    added feature to show all locations from database

--- src/main/java/com/sda/weather/backend/location/LocationController.java
+++ src/main/java/com/sda/weather/backend/location/LocationController.java
@@ -1,5 +1,9 @@
 package com.sda.weather.backend.location;
 
+import java.util.List;
+
+import java.util.List;
+
 public class LocationController {
 
     private final LocationService locationService;
@@ -11,6 +15,10 @@ public class LocationController {
     public String createNewLocation(String name, String latitude, String longitude, String country, String region) {
         Location newLocation = locationService.createNewLocation(name, latitude, longitude, country, region);
         return newLocation.toString();
+    }
+    public String displayAllLocations(){
+        List<Location> locations = locationService.displayAllLocations();
+        return locations.toString();
     }
 
 }
